d54785549486e4521213d9706169e2f6f4e91dcd
Ilya Semenov <semenov@inetss.com>
2018-05-15 23:44:31 +0700

    Allow to mount a Koa app

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import Koa from 'koa'
 import compose from 'koa-compose'
 import mount from 'koa-mount'
 
-export default function mountFinal (prefix: string, middleware: Koa.Middleware) {
-	return mount(prefix, compose([middleware, ctx => {
+export default function mountFinal (prefix: string, app: Koa.Middleware | Koa) {
+	const middlewares = app instanceof Koa ? [...app.middleware] : [app]
+	middlewares.push(ctx => {
 		// Middleware that does nothing (instead of falling through).
 		ctx.status = 404
-	}]))
+	})
+	return mount(prefix, compose(middlewares))
 }
